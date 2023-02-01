a5c8b2c8f795cc4dfc7eee652c163b0a8d4828ee
Gabriel Gosselin Roberge <gabrielgosrob@gmail.com>
2023-02-01 17:38:48 -0500

    test tailwind on log buttons

--- components/login-btn.tsx
+++ components/login-btn.tsx
@@ -4,17 +4,26 @@ export default function LoginButton() {
   const { data: session } = useSession()
   if (session) {
     return (
-      <>
+      <div>
         Signed in as {session.user?.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
+        <button
+          onClick={() => signOut()}
+          className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'
+        >
+          Sign out
+        </button>
+      </div>
     )
   }
+
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-      <div className='bg-slate-400'>allo</div>
-    </>
+    <div>
+      <button
+        onClick={() => signIn()}
+        className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'
+      >
+        Sign in
+      </button>
+    </div>
   )
 }
