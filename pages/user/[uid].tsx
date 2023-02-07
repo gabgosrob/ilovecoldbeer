5c898d03eacac40a586a5b703a157b503e48eea9
Gabriel Gosselin Roberge <gabrielgosrob@gmail.com>
2023-02-07 02:25:57 -0500

    add styling to user page

--- pages/user/[uid].tsx
+++ pages/user/[uid].tsx
@@ -3,25 +3,23 @@ import Head from 'next/head'
 import prisma from '@/lib/prismadb'
 import Image from 'next/image'
 import { UserProps } from '@/lib/custom-types'
+import Navbar from '@/components/Navbar'
 
 export default function UserPage({ user }: UserProps) {
   let userInfo
 
   if (!user) {
-    userInfo = (
-      <div>
-        <div>This user does not exist!</div>
-      </div>
-    )
+    userInfo = <div>This user does not exist!</div>
   } else {
     userInfo = (
-      <div>
-        <div>{user.name}</div>
+      <div className='flex flex-col justify-center items-center gap-6'>
+        <div>Name: {user.name}</div>
+        <div>ID: {user.id}</div>
         <Image
           src={user.image || ''}
           alt='User profile picture'
-          width={500}
-          height={500}
+          width={265}
+          height={265}
         ></Image>
       </div>
     )
@@ -30,18 +28,21 @@ export default function UserPage({ user }: UserProps) {
   return (
     <div>
       <Head>
-        <title>{user.name}</title>
-        <meta name='description' content={`Page of the user ${user.name}`} />
+        <title>User page</title>
+        <meta name='description' content='Page of a user' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main>{userInfo}</main>
+      <main className='m-3 flex flex-col justify-center items-center gap-6'>
+        <Navbar />
+        {userInfo}
+      </main>
     </div>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const userId = context.params?.bid?.toString()
+  const userId = context.params?.uid?.toString()
   const user = await prisma.user.findFirst({
     where: {
       id: userId,
