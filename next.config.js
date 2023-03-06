638b746bd4a7b3e767181a27e34c8de85293f73c
Gabriel Gosselin Roberge <gabriel.gosselin-roberge.1@ulaval.ca>
2023-03-06 15:28:29 -0500

    add csp and x-frame-options headers to all pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: `frame-ancestors 'self'`
+  }
+  
+]
+
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders
+      }
+    ]
+  },
   images: {
     remotePatterns: [
       {
